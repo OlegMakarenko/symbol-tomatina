80b303e0379743a4c21bf4f473618ec080755aaa
Oleg Makarenko <olegomm@gmail.com>
2023-08-04 19:40:36 +0300

    [website/tomatina] fix: deploy build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 const i18nConfig = require('./next-i18next.config.js'); // eslint-disable-line import/extensions
 const webpack = require('webpack');
 
-if (
-	process.env.LD_LIBRARY_PATH == null ||
-	!process.env.LD_LIBRARY_PATH.includes(
-		`${process.env.PWD}/node_modules/canvas/build/Release:`,
-	)
-) {
-	process.env.LD_LIBRARY_PATH = `${process.env.PWD
-		}/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
-
 module.exports = {
 	distDir: 'build',
 	reactStrictMode: true,
